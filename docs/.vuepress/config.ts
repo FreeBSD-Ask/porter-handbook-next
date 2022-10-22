e82ef82d93d0c3682ddc397761056032cbcc0b08
ykla <yklaxds@gmail.com>
2022-10-22 18:31:26 +0800

    测试

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,38 @@
+/**
+ * @file config.ts
+ * @brief Configuration of the book.
+ * @copyright Copyright (c) 2022 FreeBSD Chinese Community. All rights reserved.
+ */
+
+import { googleAnalyticsPlugin } from '@vuepress/plugin-google-analytics'
+import { defineUserConfig } from '@vuepress/cli'
+import { sitemapPlugin } from 'vuepress-plugin-sitemap2'
+import { seoPlugin } from 'vuepress-plugin-seo2'
+import { defaultTheme } from '@vuepress/theme-default'
+import { docsearchPlugin } from '@vuepress/plugin-docsearch'
+
+import {sideBarConfig} from './configs'
+
+export default defineUserConfig({
+  lang: 'zh-CN',
+  title: 'FreeBSD port 开发者手册',
+  description: 'FreeBSD port 开发者手册',
+
+  theme: defaultTheme({
+    logo: '/favicon.ico',
+    sidebar: sideBarConfig,
+    docsRepo: 'https://github.com/FreeBSD-Ask/porter-handbook',
+    docsBranch: 'main',
+    editLinkPattern: ':repo/edit/:branch/:path',
+    editLinkText: '在 GitHub 上编辑此页',
+    lastUpdatedText: '上次更新',
+    contributorsText: '贡献者',
+  }),
+    sitemapPlugin({
+      hostname: 'https://port.bsdcn.org',
+    }),
+    seoPlugin({
+      hostname: 'https://port.bsdcn.org',
+    }),
+  ],
+});
